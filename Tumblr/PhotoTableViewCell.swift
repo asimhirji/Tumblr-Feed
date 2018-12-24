a5901a6c04d1a3423ceee268576ebbd626e42228
Asim Hirji <asimhirji@gmail.com>
2018-12-24 02:17:32 -0600

    update

---
+++ Tumblr/PhotoTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoTableViewCell.swift
+//  Tumblr
+//
+//  Created by Jesus perez on 2/1/18.
+//  Copyright © 2018 Jesus perez. All rights reserved.
+//
+
+import UIKit
+
+
+class PhotoTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var photo: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Tumblr/PhotosViewController.swift
@@ -0,0 +1,96 @@
+//
+//  PhotosViewController.swift
+//  Tumblr
+//
+//  Created by Jesus perez on 1/31/18.
+//  Copyright © 2018 Jesus perez. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class PhotosViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var PhotoTableView: UITableView!
+    var posts: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(PhotosViewController.didPullToRefresh(_:)), for: .valueChanged)
+        PhotoTableView.insertSubview(refreshControl, at: 0)
+        PhotoTableView.delegate = self
+        PhotoTableView.dataSource = self
+
+        // Do any additional setup after loading the view.
+        // Network request snippet
+        fetchPhotos()
+        
+    }
+    
+    func fetchPhotos() {
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                self.PhotoTableView.reloadData()
+                self.refreshControl.endRefreshing()
+            }
+        }
+        task.resume()
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+        fetchPhotos()
+    }
+    
+    func tableView(_ PhotoTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ PhotoTableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = PhotoTableView.dequeueReusableCell(withIdentifier: "PhotoTableViewCell") as! PhotoTableViewCell
+        
+        // Configure YourCustomCell using the outlets that you've defined.
+        let post = posts[indexPath.row]
+        print(post)
+        if let photos = post["photos"] as? [[String: Any]] {
+            // photos is NOT nil, we can use it!
+            // TODO: Get the photo url
+            let photo = photos[0]
+            let originalSize = photo["original_size"] as! [String: Any]
+            let urlString = originalSize["url"] as! String
+            let url = URL(string: urlString)
+            
+            cell.photo.af_setImage(withURL: url!)
+        }
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
